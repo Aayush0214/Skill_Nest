d53dae3db37a117769c7a673c48791529ce1fe49
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-06-25 22:17:27 +0530

    added core folder that contains all the constants

---
+++ lib/core/theme/app_colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color appBgGrey = Color(0xffF6F6F6);
+  static Color appBgWhite = Color(0xffFEFEFE);
+  static Color appBgBlack = Color(0xff040404);
+  static Color appBgOrange = Color(0xffE46C22);
+}
