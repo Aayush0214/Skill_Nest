1fc3408f451fd1e6db50779336f76c726da38f58
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-06-25 22:15:51 +0530

    added onboard screen

--- lib/features/on_boarding/presentation/screens/on_boarding.dart
+++ lib/features/on_boarding/presentation/screens/on_boarding.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
 
-class OnBoarding extends StatelessWidget {
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
   @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: 375.w,
+        color: AppColors.appBgWhite,
+        child: Stack(
+          children: [
+            PageView(
+              children: [
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
