294e2bf4b1f018411c5337c03876ff09273ad57a
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-06-28 22:05:53 +0530

    created constants folder that contains constants image strings

---
+++ lib/core/constants/constant_images.dart
@@ -0,0 +1,5 @@
+class ConstantImages {
+  static String onBoarding1 = "assets/images/on_boarding_1.png";
+  static String onBoarding2 = "assets/images/on_boarding_2.png";
+  static String onBoarding3 = "assets/images/on_boarding_3.png";
+}
---
+++ lib/core/constants/constant_strings.dart
@@ -0,0 +1,10 @@
+class ConstantStrings {
+  static String onBoardTitle1 = "Discover Smarter Learning";
+  static String onBoardSubTitle1 = "Leave paper behind and unlock a world of knowledge—all in one place.";
+
+  static String onBoardTitle2 = "Stay Connected Effortlessly";
+  static String onBoardSubTitle2 = "Reach out to tutors and friends anytime—learning grows stronger together.";
+
+  static String onBoardTitle3 = "Learn Without Limits";
+  static String onBoardSubTitle3 = "Study whenever, wherever—your time, your pace, your freedom.";
+}
