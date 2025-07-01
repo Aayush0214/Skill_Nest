704174b231c2806f9a74564e3b77fddcc473b0fd
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-01 23:36:21 +0530

    added social images

--- lib/core/constants/constant_images.dart
+++ lib/core/constants/constant_images.dart
@@ -1,5 +1,11 @@
 class ConstantImages {
+  /// Onboarding
   static String onBoarding1 = "assets/images/on_boarding_1.png";
   static String onBoarding2 = "assets/images/on_boarding_2.png";
   static String onBoarding3 = "assets/images/on_boarding_3.png";
+
+  /// Social Icons
+  static String apple = "assets/icons/apple.png";
+  static String google = "assets/icons/google.png";
+  static String facebook = "assets/icons/facebook.png";
 }
