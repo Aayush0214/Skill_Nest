09fb46f833b26cdaf8d7f728db782da201ec528f
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-02 23:57:17 +0530

    updated login screen

--- lib/features/authentication/presentation/screen/login.dart
+++ lib/features/authentication/presentation/screen/login.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:skill_nest/core/constants/constant_images.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
 import 'package:skill_nest/core/common/widgets/common_button.dart';
@@ -21,18 +23,18 @@ class Login extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Log in', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp, color: AppColors.appBgBlack)),
+                Text('Log in', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30.sp, color: AppColors.appBgBlack)),
                 Text('Please enter your details to login.', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.sp, color: AppColors.appBgBlack.withAlpha((255 * 0.5).round()))),
                 SizedBox(height: 60),
                 
                 Text('Email', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14.sp, color: AppColors.appBgBlack.withAlpha((255 * 0.7).round()))),
                 SizedBox(height: 8),
-                formTextField(hintText: 'Enter your email', prefixIcon: Icons.email_outlined, isSuffix: false, obscureText: false),
+                formTextField(hintText: 'Enter your email', prefixIcon: Iconsax.sms, isSuffix: false, obscureText: false),
                 SizedBox(height: 30),
 
                 Text('Password', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14.sp, color: AppColors.appBgBlack.withAlpha((255 * 0.7).round()))),
                 SizedBox(height: 8),
-                formTextField(hintText: 'Enter your password', prefixIcon: Icons.lock_outline, isSuffix: true, suffixIcon: Icons.remove_red_eye, obscureText: true),
+                formTextField(hintText: 'Enter your password', prefixIcon: Iconsax.lock_1, isSuffix: true, suffixIcon: Icons.remove_red_eye, obscureText: true),
                 SizedBox(height: 10),
                 
                 TextButton(
@@ -53,16 +55,29 @@ class Login extends StatelessWidget {
                 SizedBox(height: 40),
 
                 authDivider(),
-
                 SizedBox(height: 40),
+
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    socialContainer(ConstantImages.apple),
-                    socialContainer(ConstantImages.google),
-                    socialContainer(ConstantImages.facebook),
+                    socialContainer(ConstantImages.google, (){}),
+                    socialContainer(ConstantImages.apple, (){}),
+                    socialContainer(ConstantImages.facebook, (){}),
                   ],
-                )
+                ),
+                SizedBox(height: 40),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?", style: TextStyle(fontWeight: FontWeight.normal, color: Colors.grey, fontSize: 14.sp)),
+                    TextButton(
+                      onPressed: (){},
+                      child: Text('SignUp', style: TextStyle(fontWeight: FontWeight.w600, color: AppColors.appBgOrange, fontSize: 15.sp)),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
--- lib/features/authentication/presentation/screen/widgets/social_icon.dart
+++ lib/features/authentication/presentation/screen/widgets/social_icon.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
 
-Widget socialContainer(String image) {
+Widget socialContainer(String image, VoidCallback onTap) {
   return GestureDetector(
-    onTap: (){},
+    onTap: onTap,
     child: Container(
       width: 45.w,
       height: 45.w,
-      padding: EdgeInsets.all(8),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
+          shape: BoxShape.circle,
           color: AppColors.appBgWhite,
-          borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
+              blurRadius: 2,
               spreadRadius: 1,
-              blurRadius: 2.5,
+              blurStyle: BlurStyle.outer,
               color: Colors.grey.shade400,
             ),
           ]
