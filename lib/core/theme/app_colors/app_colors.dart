1370110b7082971e88b64a457622e32f00a2750c
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-03 11:33:14 +0530

    added new colors

--- lib/core/theme/app_colors/app_colors.dart
+++ lib/core/theme/app_colors/app_colors.dart
@@ -1,8 +1,13 @@
 import 'dart:ui';
 
 class AppColors {
-  static Color appBgGrey = Color(0xffF6F6F6);
-  static Color appBgWhite = Color(0xffFEFEFE);
-  static Color appBgBlack = Color(0xff040404);
-  static Color appBgOrange = Color(0xffE46C22);
+
+  static const Color red = Color(0xffd90429);
+  static const Color grey = Color(0xffbcb8b1);
+  static const Color white = Color(0xffFEFEFE);
+  static const Color black = Color(0xff000000);
+  static const Color primary = Color(0xffE46C22);
+
+  /// Background colors
+  static const Color appBgGrey = Color(0xffF6F6F6);
 }
