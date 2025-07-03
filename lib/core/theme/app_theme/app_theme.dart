0ca33d33b9c97663a2daf11ffaddfa4117a3019e
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-03 11:31:29 +0530

    created a separate theme folder for better scalability

---
+++ lib/core/theme/app_theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static _border([Color color = AppColors.black]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15),
+    borderSide: BorderSide(color: color),
+  );
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'SofiaSans',
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.appBgGrey,
+    appBarTheme: AppBarTheme(backgroundColor: AppColors.appBgGrey),
+    inputDecorationTheme: InputDecorationTheme(
+      border: _border(),
+      focusedBorder: _border(),
+      errorBorder: _border(AppColors.red),
+      hintStyle: TextStyle(color: AppColors.grey, fontWeight: FontWeight.w400),
+    ),
+  );
+}
