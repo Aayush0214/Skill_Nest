4a6f4f3a43681b32abeeee09e92424aa2eb2b976
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-05 23:00:45 +0530

    added loginEvent

--- lib/features/authentication/presentation/bloc/auth_bloc.dart
+++ lib/features/authentication/presentation/bloc/auth_bloc.dart
@@ -6,8 +6,16 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
+    on<AuthEvent>((event, emit) {});
+    on<AuthLoginEvent>((event, emit) {
+      debugPrint('AuthLoginEvent called');
+      debugPrint('email: ${event.email}');
+      debugPrint('password: ${event.password}');
+      if (event.email.isNotEmpty && event.password.isNotEmpty) {
 
+      } else {
+        emit(AuthFailureState(message: 'Email & Password is required!!'));
+      }
     });
   }
 }
--- lib/features/authentication/presentation/bloc/auth_state.dart
+++ lib/features/authentication/presentation/bloc/auth_state.dart
@@ -4,3 +4,9 @@ part of 'auth_bloc.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class AuthFailureState extends AuthState {
+  final String message;
+
+  AuthFailureState({required this.message});
+}
