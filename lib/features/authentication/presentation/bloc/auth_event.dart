dcbf9564f197e9d8f3b347182a8c838ad98314e4
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-06 13:16:33 +0530

    added show password even

--- lib/features/authentication/presentation/bloc/auth_event.dart
+++ lib/features/authentication/presentation/bloc/auth_event.dart
@@ -3,6 +3,12 @@ part of 'auth_bloc.dart';
 @immutable
 sealed class AuthEvent {}
 
+final class ShowPasswordEvent extends AuthEvent {
+  final bool showPassword;
+
+  ShowPasswordEvent({required this.showPassword});
+}
+
 final class AuthLoginEvent extends AuthEvent {
   final String email;
   final String password;
