1247232739613da7c14c8e562563a8cb64a9b6b5
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-06 13:17:19 +0530

    added show password state

--- lib/features/authentication/presentation/bloc/auth_state.dart
+++ lib/features/authentication/presentation/bloc/auth_state.dart
@@ -5,6 +5,12 @@ sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
 
+final class ShowPasswordState extends AuthState {
+  final bool showPassword;
+
+  ShowPasswordState({required this.showPassword});
+}
+
 final class AuthFailureState extends AuthState {
   final String message;
 
