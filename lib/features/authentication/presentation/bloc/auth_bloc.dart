499ff6711bab02cf21e926658adbd06dc2553877
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-06 13:15:55 +0530

    cleaned code

--- lib/features/authentication/presentation/bloc/auth_bloc.dart
+++ lib/features/authentication/presentation/bloc/auth_bloc.dart
@@ -7,15 +7,18 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
     on<AuthEvent>((event, emit) {});
-    on<AuthLoginEvent>((event, emit) {
-      debugPrint('AuthLoginEvent called');
-      debugPrint('email: ${event.email}');
-      debugPrint('password: ${event.password}');
-      if (event.email.isNotEmpty && event.password.isNotEmpty) {
-
-      } else {
-        emit(AuthFailureState(message: 'Email & Password is required!!'));
-      }
+    on<ShowPasswordEvent> ((event, emit) {
+      emit(ShowPasswordState(showPassword: !event.showPassword));
     });
+    on<AuthLoginEvent>(_loginEvent);
+  }
+
+
+  void _loginEvent(AuthLoginEvent event, Emitter<AuthState> emit) {
+    if (event.email.isNotEmpty && event.password.isNotEmpty) {
+
+    } else {
+      emit(AuthFailureState(message: 'Email & Password is required!!'));
+    }
   }
 }
