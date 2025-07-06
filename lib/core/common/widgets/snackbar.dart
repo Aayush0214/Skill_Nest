5ebb9fb99ebf4794c79c6cd680bd5594e66a56fc
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-06 09:53:52 +0530

    common snackbar

---
+++ lib/core/common/widgets/snackbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
+
+void showSnackBar({required BuildContext context, required String content, required IconData icon, required Color color}) {
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        elevation: 0,
+        duration: Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        margin: EdgeInsets.symmetric(horizontal: 1.w, vertical: 1.h),
+        content: Container(
+          height: 40.h,
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 5.h),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: color.withAlpha(20),
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: color.withAlpha(100), width: 2),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 30.w,
+                height: 30.w,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Icon(icon, color: Colors.white),
+              ),
+              SizedBox(width: 10),
+              Text(content, softWrap: true, maxLines: 3, overflow: TextOverflow.ellipsis, style: TextStyle(color: AppColors.black, fontWeight: FontWeight.w500, fontSize: 14.sp)),
+            ],
+          ),
+        ),
+      ),
+    );
+}
