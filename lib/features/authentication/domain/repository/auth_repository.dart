34879217d1caa31391635e05465efa8110ebd614
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-07 19:30:32 +0530

    auth repository class with abstract methods

--- lib/features/authentication/domain/repository/auth_repository.dart
+++ lib/features/authentication/domain/repository/auth_repository.dart
@@ -1,19 +1,24 @@
-import '../entity/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../entity/user_entity.dart';
 import 'package:fpdart/fpdart.dart';
 import '../../../../core/error/failure.dart';
 
 abstract interface class AuthRepository {
-  Future<Either<Failure, User>> loginWithEmailPassword({
+
+  Stream<User?> get authStateChanges;
+
+  Future<Either<Failure, UserEntity>> loginWithEmailPassword({
     required String email,
     required String password,
   });
 
-  Future<Either<Failure, User>> signUpWithEmailPassword({
+  Future<Either<Failure, UserEntity>> signUpWithEmailPassword({
     required String email,
     required String password,
   });
 
-  Future<Either<Failure, User>> signInWithGoogle();
+  Future<Either<Failure, UserEntity>> signInWithGoogle();
 
   Future<Either<Failure, void>> sendEmailVerification();
 }
