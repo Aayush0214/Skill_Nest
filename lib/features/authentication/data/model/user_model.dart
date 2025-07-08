0e43717c798b74d686f647c6a13232712ddd4935
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-08 18:00:21 +0530

    added some more user information fields

--- lib/features/authentication/data/model/user_model.dart
+++ lib/features/authentication/data/model/user_model.dart
@@ -1,10 +1,20 @@
-import 'package:firebase_auth/firebase_auth.dart' as firebase;
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skill_nest/features/authentication/domain/entity/user_entity.dart';
 
 class UserModel extends UserEntity {
-  const UserModel({required super.uid, super.email});
+  const UserModel({
+    required super.uid,
+    required super.email,
+    required super.userName,
+    required super.isEmailVerified,
+  });
 
-  factory UserModel.fromFirebase(firebase.User user) {
-    return UserModel(uid: user.uid, email: user.email);
+  factory UserModel.fromFirebase(User user) {
+    return UserModel(
+      uid: user.uid,
+      email: user.email ?? '',
+      userName: user.displayName ?? '',
+      isEmailVerified: user.emailVerified,
+    );
   }
 }
--- lib/features/authentication/domain/entity/user_entity.dart
+++ lib/features/authentication/domain/entity/user_entity.dart
@@ -1,6 +1,13 @@
 class UserEntity{
   final String uid;
-  final String? email;
+  final String email;
+  final String userName;
+  final bool isEmailVerified;
 
-  const UserEntity({required this.uid, this.email});
+  const UserEntity({
+    required this.uid,
+    required this.email,
+    required this.userName,
+    required this.isEmailVerified,
+  });
 }
