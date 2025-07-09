79303bf6e916a72c881e663174b12d8d4907773e
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-09 20:56:21 +0530

    written all neccessary strings

--- lib/core/constants/constant_strings.dart
+++ lib/core/constants/constant_strings.dart
@@ -1,4 +1,6 @@
 class ConstantStrings {
+
+  /// ------------- Onboarding Screen Texts -------------
   static String onBoardTitle1 = "Discover Smarter Learning";
   static String onBoardSubTitle1 = "Leave paper behind and unlock a world of knowledge—all in one place.";
 
@@ -7,4 +9,29 @@ class ConstantStrings {
 
   static String onBoardTitle3 = "Learn Without Limits";
   static String onBoardSubTitle3 = "Study whenever, wherever—your time, your pace, your freedom.";
+
+  static String emailNotVerifiedHeading = "* Email not verified! *";
+  static String emailNotVerifiedInfo = "Looks like your email is not verified. Please verify your email. "
+      "Send confirm email request for email confirmation link to your registered email address.";
+
+  /// ------------- Login Screen Texts -------------
+  static String loginText = "Login";
+  static String registerButtonText = "Register";
+  static String forgotPasswordText = "Forgot Password?";
+  static String notRegisteredText = "Haven't registered yet?";
+  static String loginScreenHeading = "Log in to your \nAccount";
+  static String loginScreenSubHeading = "Please enter your details to login.";
+
+  /// ------------- Signup Screen Texts -------------
+  static String signinText = "Sign-In";
+  static String signupScreenHeading = "Create your account";
+  static String alreadyHaveAccount = "Already have an account?";
+  static String signupScreenSubHeading = "Please enter your details to register.";
+
+  /// ------------- Verify email Screen Texts -------------
+  static String checkYourMail = "* Check your mail *";
+  static String verifyEmailButtonText = "Verify Email";
+  static String verifyEmailPageContent = "Thanks for Signup with us. Click on the button below to verify your email address.";
+  static String checkYourMailMessage = "We just sent you a email verification link to your registered email address."
+      "We need to verify your email in order to continue your registration. In case you don't find email please check your spam folder";
 }
