1918e6f96b1b4e9986582d5e63b1aa7f15d5decf
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-09 21:09:43 +0530

    added some more fields to show userinfo

--- lib/features/authentication/domain/entity/user_entity.dart
+++ lib/features/authentication/domain/entity/user_entity.dart
@@ -1,13 +1,13 @@
 class UserEntity{
   final String uid;
   final String email;
-  final String userName;
+  final String username;
   final bool isEmailVerified;
 
   const UserEntity({
     required this.uid,
     required this.email,
-    required this.userName,
+    required this.username,
     required this.isEmailVerified,
   });
 }
