053da69c4b776c07d7fc241d7813c97610da11e1
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-11 23:49:00 +0530

    changed code for saving details using shared preferences

--- lib/core/services/local_storage_service/data/repository_impl/local_storage_impl.dart
+++ lib/core/services/local_storage_service/data/repository_impl/local_storage_impl.dart
@@ -1,40 +1,18 @@
 import '../../domain/repository/local_storage.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorageImpl implements LocalStorage {
 
-  final FlutterSecureStorage _storage = const FlutterSecureStorage(
-    aOptions: AndroidOptions(encryptedSharedPreferences: true),
-    iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
-  );
+  final SharedPreferences prefs;
+  static const _onboardingKey = 'onboarding_shown';
+
+  LocalStorageImpl({required this.prefs});
 
   @override
-  Future<void> writeData({required String key, required String value}) async {
-    await _storage.write(key: key, value: value);
-  }
+  Future<bool> get isOnboardingShown async => prefs.getBool(_onboardingKey) ?? false;
 
   @override
-  Future<String?> readData({required String key}) async {
-    return await _storage.read(key: key);
-  }
-
-  @override
-  Future<void> deleteData({required String key}) async {
-    await _storage.delete(key: key);
-  }
-
-  @override
-  Future<void> deleteAllData() async {
-    await _storage.deleteAll();
-  }
-
-  @override
-  Future<bool> containsKey({required String key}) async {
-    return await _storage.containsKey(key: key);
-  }
-
-  @override
-  Future<Map<String, String>> readAllData() async {
-    return await _storage.readAll();
+  Future<void> setOnboardingShown() async {
+    await prefs.setBool(_onboardingKey, true);
   }
 }
