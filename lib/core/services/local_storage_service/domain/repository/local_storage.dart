b22496717dfcae510ce41cbac74c06bc2e7ee6c6
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-11 23:49:35 +0530

    updated local storage repository class

--- lib/core/services/local_storage_service/domain/repository/local_storage.dart
+++ lib/core/services/local_storage_service/domain/repository/local_storage.dart
@@ -1,8 +1,4 @@
 abstract class LocalStorage {
-  Future<void> writeData({required String key, required String value});
-  Future<String?> readData({required String key});
-  Future<void> deleteData({required String key});
-  Future<void> deleteAllData();
-  Future<bool> containsKey({required String key});
-  Future<Map<String, String>> readAllData();
+  Future<bool> get isOnboardingShown;
+  Future<void> setOnboardingShown();
 }
