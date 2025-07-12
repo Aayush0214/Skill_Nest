644b5be725750eb2b423c098d77535e536f3f966
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-12 20:06:41 +0530

    implemented proper navigation based on user state

--- lib/features/splash/presentation/screen/splash.dart
+++ lib/features/splash/presentation/screen/splash.dart
@@ -4,12 +4,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skill_nest/core/common/widgets/snackbar.dart';
 import 'package:skill_nest/core/constants/constant_images.dart';
 import 'package:skill_nest/core/constants/constant_strings.dart';
-import 'package:skill_nest/core/services/navigation_service/navigation_service.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
-import 'package:skill_nest/features/authentication/presentation/screen/pages/login.dart';
-import 'package:skill_nest/features/dashboard/presentation/pages/dashboard.dart';
-import 'package:skill_nest/features/on_boarding/presentation/screens/pages/on_boarding.dart';
 import 'package:skill_nest/features/splash/presentation/bloc/splash_bloc.dart';
+import 'package:skill_nest/features/dashboard/presentation/pages/dashboard.dart';
+import 'package:skill_nest/core/services/navigation_service/navigation_service.dart';
+import 'package:skill_nest/features/authentication/presentation/screen/pages/login.dart';
+import 'package:skill_nest/features/on_boarding/presentation/screens/pages/on_boarding.dart';
+import 'package:skill_nest/features/authentication/presentation/screen/pages/verify_email.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -22,6 +23,8 @@ class SplashScreen extends StatelessWidget {
           NavigationService.pushReplacement(context, OnBoarding());
         } else if (state is NavigateToDashboardState) {
           NavigationService.pushReplacement(context, Dashboard());
+        } else if (state is NavigateToVerifyEmailState) {
+          NavigationService.pushReplacement(context, VerifyEmail());
         } else if (state is NavigateToLoginState) {
           if (state.message!.isNotEmpty) {
             showSnackBar(context: context, color: AppColors.red, content: state.message!, icon: Icons.report_gmailerrorred);
