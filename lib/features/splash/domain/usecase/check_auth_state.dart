1214d849b2a060b3c438da6e28ee98da6a28bc0a
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-12 20:02:28 +0530

    implemented stream listening methods to know user current state

--- lib/features/splash/domain/usecase/check_auth_state.dart
+++ lib/features/splash/domain/usecase/check_auth_state.dart
@@ -1,26 +1,14 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skill_nest/core/error/failure.dart';
-import 'package:skill_nest/core/usecase/usecase.dart';
 import 'package:skill_nest/features/splash/domain/repository/splash_repository.dart';
-
-// class CheckAuthStateUsecase implements UseCase<User?, NoParams>{
-//   final SplashRepository repository;
-//
-//   CheckAuthStateUsecase({required this.repository});
-//
-//   @override
-//   Future<Either<Failure, User?>> call(NoParams params) async {
-//     return await repository.isUserExist();
-//   }
-// }
 
 class CheckAuthStateUsecase {
   final SplashRepository repository;
 
   CheckAuthStateUsecase({required this.repository});
 
-  Stream<User?> call() async*{
-    yield* repository.userState();
+  Stream<Either<Failure, User?>> call() {
+    return repository.userState();
   }
 }
