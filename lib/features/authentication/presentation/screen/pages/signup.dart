a015c2172280922b19f582c899b2858710ff742f
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-12 19:38:00 +0530

    final changes made

--- lib/features/authentication/presentation/screen/pages/signup.dart
+++ lib/features/authentication/presentation/screen/pages/signup.dart
@@ -4,14 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skill_nest/core/common/widgets/snackbar.dart';
 import 'package:skill_nest/core/constants/constant_strings.dart';
-import 'package:skill_nest/core/services/navigation_service/navigation_service.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
 import 'package:skill_nest/core/common/widgets/common_button.dart';
 import 'package:skill_nest/core/common/widgets/loading_dialog.dart';
+import 'package:skill_nest/core/common/widgets/form_textfield.dart';
+import 'package:skill_nest/core/services/navigation_service/navigation_service.dart';
 import 'package:skill_nest/features/authentication/presentation/bloc/auth_bloc.dart';
 import 'package:skill_nest/features/authentication/presentation/screen/pages/login.dart';
 import 'package:skill_nest/features/authentication/presentation/screen/pages/verify_email.dart';
-import 'package:skill_nest/features/authentication/presentation/screen/widgets/form_textfield.dart';
 import 'package:skill_nest/features/authentication/presentation/screen/widgets/textfield_heading.dart';
 
 class SignUp extends StatefulWidget {
