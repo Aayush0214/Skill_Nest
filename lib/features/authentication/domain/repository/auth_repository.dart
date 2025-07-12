a4302baf5ad0d1b1cc8c5ea8967e915c59790b38
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-12 19:57:13 +0530

    removed unwanted stream methods

--- lib/features/authentication/domain/repository/auth_repository.dart
+++ lib/features/authentication/domain/repository/auth_repository.dart
@@ -1,12 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 import '../entity/user_entity.dart';
 import 'package:fpdart/fpdart.dart';
 import '../../../../core/error/failure.dart';
 
 abstract interface class AuthRepository {
-
-  Stream<User?> get authStateChanges;
 
   Future<Either<Failure, UserEntity>> loginWithEmailPassword({
     required String email,
