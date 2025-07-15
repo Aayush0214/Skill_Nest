f1569b3a50e064c02e678b053c0781b252714016
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-15 10:08:06 +0530

    changed size of the dot indicators

--- lib/core/common/widgets/dot_indicator.dart
+++ lib/core/common/widgets/dot_indicator.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
 
-Widget onboardingDotIndicator(double currentIndex) {
+Widget dotIndicator(double currentIndex, {int count = 3}) {
   return  DotsIndicator(
-    dotsCount: 3,
     animate: true,
+    dotsCount: count,
     axis: Axis.horizontal,
     position: currentIndex,
     mainAxisAlignment: MainAxisAlignment.center,
     decorator: DotsDecorator(
-      size: const Size.square(10),
-      activeSize: const Size(25, 10),
+      size: const Size.square(7),
+      activeSize: const Size(28, 7),
       activeColor: AppColors.primary,
       activeShape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(5),
