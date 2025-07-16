405c37d93eca4da0a132d00895d5896a142ea532
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-16 10:12:49 +0530

    reusable gridview

---
+++ lib/features/home/presentation/widgets/home_courses_grid_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import '../../domain/entity/dummy_entity.dart';
+import '../../../../core/constants/constant_images.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../core/theme/app_colors/app_colors.dart';
+
+Widget courses({required List<Course> filteredCourses}) {
+  return SliverPadding(
+    padding: EdgeInsets.fromLTRB(5.w, 1.h, 5.w, 8.h),
+    sliver: SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        childAspectRatio: 1.07,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        childCount: filteredCourses.length,
+            (context, index) {
+          final course = filteredCourses[index]; // Assuming you have this in your state
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 4.h),
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(10.w),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 1,
+                    color: AppColors.grey,
+                  )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadiusDirectional.circular(10.w),
+                    child: Image.asset(
+                      height: 90.h,
+                      fit: BoxFit.fill,
+                      ConstantImages.course,
+                      width: double.infinity,
+                    ),
+                  ),
+                  SizedBox(height: 5.h),
+                  Expanded(
+                    child: Text(
+                      maxLines: 2,
+                      course.title,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.black.withAlpha(180),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
