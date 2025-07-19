b1a672e2ef35431416574760262e3128c822a1c7
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-19 15:25:21 +0530

    changes made in search box and filter button

--- lib/features/home/presentation/widgets/home_courses_grid_view.dart
+++ lib/features/home/presentation/widgets/home_courses_grid_view.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import '../../domain/entity/dummy_entity.dart';
-import '../../../../core/constants/constant_images.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../../core/theme/app_colors/app_colors.dart';
+import 'package:skill_nest/core/common/widgets/container.dart';
+import 'package:skill_nest/core/constants/constant_images.dart';
+import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
+import 'package:skill_nest/features/home/domain/entity/dummy_entity.dart';
 
 Widget courses({required List<Course> filteredCourses}) {
   return SliverPadding(
@@ -14,56 +15,46 @@ Widget courses({required List<Course> filteredCourses}) {
         crossAxisSpacing: 8,
         childAspectRatio: 1.07,
       ),
-      delegate: SliverChildBuilderDelegate(
-        childCount: filteredCourses.length,
-            (context, index) {
-          final course = filteredCourses[index]; // Assuming you have this in your state
-          return GestureDetector(
-            onTap: () {},
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 4.h),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10.w),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 1,
-                    color: AppColors.grey,
-                  )
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadiusDirectional.circular(10.w),
-                    child: Image.asset(
-                      height: 90.h,
-                      fit: BoxFit.fill,
-                      ConstantImages.course,
-                      width: double.infinity,
+      delegate: SliverChildBuilderDelegate(childCount: filteredCourses.length, (
+        context,
+        index,
+      ) {
+        final course =
+            filteredCourses[index]; // Assuming you have this in your state
+        return GestureDetector(
+          onTap: () {},
+          child: commonContainer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadiusDirectional.circular(10.w),
+                  child: Image.asset(
+                    height: 90.h,
+                    fit: BoxFit.fill,
+                    ConstantImages.course,
+                    width: double.infinity,
+                  ),
+                ),
+                SizedBox(height: 5.h),
+                Expanded(
+                  child: Text(
+                    maxLines: 2,
+                    course.title,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.black.withAlpha(180),
                     ),
                   ),
-                  SizedBox(height: 5.h),
-                  Expanded(
-                    child: Text(
-                      maxLines: 2,
-                      course.title,
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.black.withAlpha(180),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      }),
     ),
   );
 }
