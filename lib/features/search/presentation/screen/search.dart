c738a15ce1eb481a0642b91de3c84830fb1dd5ce
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-20 22:45:03 +0530

    updated the list of searched course

--- lib/features/search/presentation/screen/search.dart
+++ lib/features/search/presentation/screen/search.dart
@@ -1,11 +1,8 @@
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skill_nest/core/common/widgets/appbar.dart';
-import 'package:skill_nest/core/common/widgets/custom_text.dart';
 import 'package:skill_nest/core/constants/constant_images.dart';
-import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
+import 'package:skill_nest/core/common/widgets/course_tile.dart';
 import 'package:skill_nest/core/common/widgets/search_and_filter.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -28,7 +25,7 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: commonAppBar('My Profile'),
+      appBar: commonAppBar('Search'),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
@@ -45,7 +42,11 @@ class _SearchScreenState extends State<SearchScreen> {
                   courseTitle: 'Flutter Firebase combined course',
                   subTitle: 'Flutter Firebase combined course Flutter Firebase combined course',
                   courseImage: ConstantImages.course,
-                  totalRating: 3.5,
+                  coursePrice: '549₹',
+                  tutorImage: ConstantImages.profileImage,
+                  tutorName: 'Aayush Kushwaha',
+                  subTitleMaxLine: 1,
+                  rating: '4.1',
                   onListTap: (){},
                 ),
               ),
@@ -55,69 +56,4 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
     );
   }
-}
-
-Widget courseList({
-  required String courseTitle,
-  required String subTitle,
-  required String courseImage,
-  required double totalRating,
-  VoidCallback? onListTap,
-}) {
-  return ListTile(
-    onTap: onListTap,
-    tileColor: AppColors.white,
-    contentPadding: EdgeInsets.symmetric(horizontal: 5.w),
-    leading: Container(
-      width: 50.h,
-      height: 120.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.w),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadiusDirectional.circular(10.w),
-        child: Image.asset(
-          courseImage,
-          fit: BoxFit.fill,
-          width: double.infinity,
-        ),
-      ),
-    ),
-    title: customText(
-      fontSize: 14.sp,
-      textString: courseTitle,
-      textColor: AppColors.black,
-      fontWeight: FontWeight.w600,
-    ),
-    subtitle: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          subTitle,
-          maxLines: 1,
-          softWrap: true,
-          textAlign: TextAlign.start,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 12.sp,
-            color: AppColors.grey,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        RatingBarIndicator(
-          rating: totalRating,               // double हो सकता है: 3.5, 4.2, etc.
-          itemCount: 5,
-          itemSize: 10.h,
-          direction: Axis.horizontal,
-          itemBuilder: (context, _) => const Icon(
-            Icons.star,
-            color: Colors.amber,
-          ),
-        )
-      ],
-    ),
-    trailing: Icon(Iconsax.arrow_right_3, color: AppColors.black),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.h)),
-  );
 }
