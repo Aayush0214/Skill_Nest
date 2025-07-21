9f3a4b798940bd3f7b64d313bc6afaa87298741a
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-21 21:30:15 +0530

    added video player bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:skill_nest/core/services/notification_service/notification_service.dart';
+import 'package:flutter/services.dart';
 
 import 'injection.dart';
 import 'firebase_options.dart';
@@ -13,13 +13,20 @@ import 'package:skill_nest/features/splash/presentation/bloc/splash_bloc.dart';
 import 'package:skill_nest/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:skill_nest/features/authentication/presentation/bloc/auth_bloc.dart';
 import 'package:skill_nest/features/dashboard/presentation/bloc/dashboard_bloc.dart';
+import 'features/course_details/presentation/bloc/video_player/course_playback_bloc.dart';
 import 'package:skill_nest/features/on_boarding/presentation/bloc/onboarding_bloc.dart';
+import 'package:skill_nest/core/services/notification_service/notification_service.dart';
+import 'package:skill_nest/features/course_details/presentation/bloc/course_details_bloc/course_details_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initDependencies();
   await NotificationService.instance.initialize();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     MultiBlocProvider(
       providers: [
@@ -29,6 +36,10 @@ void main() async {
         BlocProvider(create: (_) => sl<DashboardBloc>()),
         BlocProvider(create: (_) => sl<HomeBloc>()),
         BlocProvider(create: (_) => sl<ProfileBloc>()),
+        // search bloc remaining
+        // course bloc remaining
+        BlocProvider(create: (_) => CourseDetailsBloc()),
+        BlocProvider(create: (_) => CoursePlaybackBloc()),
       ],
       child: const MyApp(),
     ),
