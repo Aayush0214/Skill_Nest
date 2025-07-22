6ea5e34d318c972e67279605ea6229166efa2a8d
Aayush Kushwaha <aayush02kushwaha@gmail.com>
2025-07-22 22:45:35 +0530

    updated course tile widget with actual data parameter

--- lib/core/common/widgets/course_tile.dart
+++ lib/core/common/widgets/course_tile.dart
@@ -1,18 +1,19 @@
+import 'custom_text.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:skill_nest/core/theme/app_colors/app_colors.dart';
-
-import 'custom_text.dart';
+import 'package:skill_nest/core/common/widgets/common_thumbnail.dart';
 
 Widget courseList({
+  required bool showPrice,
   required String courseTitle,
   required String subTitle,
   required String courseImage,
   required String tutorImage,
   required String tutorName,
   required String coursePrice,
-  required String rating,
+  required double rating,
   int? subTitleMaxLine = 2,
   VoidCallback? onListTap,
 }) {
@@ -23,19 +24,11 @@ Widget courseList({
     leading: Container(
       width: 60.h,
       height: 60.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.w),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadiusDirectional.circular(10.w),
-        child: Image.asset(
-          courseImage,
-          fit: BoxFit.fill,
-          width: double.infinity,
-        ),
-      ),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.w)),
+      child: commonThumbnailImage(clipRadius: 10.w, thumbnailUrl: courseImage),
     ),
     title: customText(
+      maxLines: 1,
       fontSize: 14.sp,
       textString: courseTitle,
       textColor: AppColors.black,
@@ -45,42 +38,53 @@ Widget courseList({
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          subTitle,
-          maxLines: subTitleMaxLine,
-          softWrap: true,
-          textAlign: TextAlign.start,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 12.sp,
-            color: AppColors.grey,
-            fontWeight: FontWeight.w500,
-          ),
+        customText(
+          maxLines: 2,
+          fontSize: 12.sp,
+          textString: subTitle,
+          textColor: AppColors.grey,
+          fontWeight: FontWeight.w500,
         ),
+        SizedBox(height: 8.h),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CircleAvatar(
-              radius: 10,
+              radius: 10.w,
               backgroundColor: AppColors.grey,
-              backgroundImage: NetworkImage(tutorImage),
+              child: commonNetworkProfile(profileUrl: tutorImage, clipRadius: 10.w),
             ),
             SizedBox(width: 5.w),
-            customText(textString: tutorName, fontSize: 12.sp, fontWeight: FontWeight.w500, textColor: AppColors.black, maxLines: 1),
+            customText(
+              textString: tutorName,
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w500,
+              textColor: AppColors.black,
+              maxLines: 1,
+            ),
           ],
         ),
+        SizedBox(height: 8.h),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            customText(textString: coursePrice, textColor: AppColors.black, maxLines: 1, fontWeight: FontWeight.w700, fontSize: 14.sp),
-            SizedBox(width: 8.w),
-            Icon(Icons.star, color: Colors.yellow),
+            showPrice ? customText(
+              textString: coursePrice,
+              textColor: AppColors.black,
+              maxLines: 1,
+              fontWeight: FontWeight.w700,
+              fontSize: 14.sp,
+            ) : SizedBox.shrink(),
+            Icon(Icons.star, color: Colors.orange),
             SizedBox(width: 2.w),
-            customText(textString: rating, fontWeight: FontWeight.normal)
+            customText(
+              textString: rating.toStringAsFixed(1),
+              fontWeight: FontWeight.normal,
+            ),
           ],
-        )
+        ),
       ],
     ),
     trailing: Icon(Iconsax.arrow_right_3, color: AppColors.black),
